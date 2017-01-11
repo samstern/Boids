3524815529e82b0b1c5a730af959ca485f09fd8a
samstern <samuelostern@gmail.com>
2017-01-11 11:29:49 +0000

    update methods no longer private

--- Boids/flock.py
+++ Boids/flock.py
@@ -8,16 +8,16 @@ class Flock():
 
     def update_boids(self):
         #update positions only after all boids' velocities have been adjusted
-        self.__update_velocities()
-        self.__updat_positions()
+        self._update_velocities()
+        self._update_positions()
 
-    def __update_velocities(self):
+    def _update_velocities(self):
         for boid in self.boids:
             for neighbour in self.boids:
                 boid.towardsMiddle(neighbour,self.num_boids)
                 boid.awayFromNeighbour(neighbour)
                 boid.matchSpeedOfNeighbour(neighbour,self.num_boids)
 
-    def __updat_positions(self):
+    def _update_positions(self):
         for boid in self.boids:
             boid.move()
